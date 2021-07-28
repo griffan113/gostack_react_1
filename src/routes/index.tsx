d375126a451e7d146cb6122877f1175b5930dc8a
Eliel M. Gaspar <elielmichel@gmail.com>
2021-07-28 18:59:19 -0300

    style: add .editorconfig

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,15 @@
+import { Switch, Route } from "react-router-dom";
+
 import React from "react";
+import Dashboard from "../pages/Dashboard";
+import Repository from "../pages/Repository";
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/">
+      <Dashboard />
+    </Route>
+  </Switch>
+);
+
+export default Routes;
